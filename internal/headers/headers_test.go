6573bfd356df34c183c363be1cf4bdaf190b3c4a
Shayan Siddiqui <shayansiddiqui172@gmail.com>
2026-03-04 22:59:23 -0500

    Update test cases to use localhost:10702

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -10,27 +10,27 @@ import (
 func TestParseHeaders(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
-	data := []byte("Host: localhost:42069\r\n\r\n")
+	data := []byte("Host: localhost:10702\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
-	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "localhost:10702", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
 	// Test: Valid single header with extra whitespace
 	headers = NewHeaders()
-	data = []byte("Host:     localhost:42069   \r\n\r\n")
+	data = []byte("Host:     localhost:10702   \r\n\r\n")
 	_, done, err = headers.Parse(data)
 	require.NoError(t, err)
-	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "localhost:10702", headers["host"])
 	assert.False(t, done)
 
 	// Test: Valid 2 headers with existing headers
 	headers = NewHeaders()
-	data = []byte("Host: localhost:42069\r\nUser-Agent: curl/7.81.0\r\n\r\n")
+	data = []byte("Host: localhost:10702\r\nUser-Agent: curl/7.81.0\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
-	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "localhost:10702", headers["host"])
 	assert.False(t, done)
 	n, done, err = headers.Parse(data[n:])
 	require.NoError(t, err)
@@ -47,7 +47,7 @@ func TestParseHeaders(t *testing.T) {
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
-	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	data = []byte("       Host : localhost:10702       \r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
@@ -55,7 +55,7 @@ func TestParseHeaders(t *testing.T) {
 
 	// Test: Invalid character in header key
 	headers = NewHeaders()
-	data = []byte("H©st: localhost:42069\r\n\r\n")
+	data = []byte("H©st: localhost:10702\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
